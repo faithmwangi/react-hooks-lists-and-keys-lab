9ae55e8ffee8ce2d84dc361ca924ebcbda853a05
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-04-26 11:33:19 +0300

    Complete assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, description, dueDate, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
-      <p>{about}</p>
+      <p>{description}</p>
+      <p>Due Date: {dueDate}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology, index) => (
+          <span key={index}>{technology}</span>
+        ))}
       </div>
     </div>
   );
